e6a1cd5c2b5aa7ab777aa3ca81854275975fe136
Chern Lai <chernobyl@Cherns-MacBook-Pro.local>
2015-06-23 21:05:05 -0700

    updates

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
 	println!("cargo:rustc-link-search=native=lib");
 	println!("cargo:rustc-link-search=native=/usr/lib");
-	println!("cargo:rustc-link-lib=static=BulletDynamics_gmake_x64_debug");
-	println!("cargo:rustc-link-lib=static=BulletCollision_gmake_x64_debug");
-	println!("cargo:rustc-link-lib=static=LinearMath_gmake_x64_debug");
+	println!("cargo:rustc-link-lib=static=BulletDynamics_xcode4_x64_release");
+	println!("cargo:rustc-link-lib=static=BulletCollision_xcode4_x64_release");
+	println!("cargo:rustc-link-lib=static=LinearMath_xcode4_x64_release");
 	println!("cargo:rustc-link-lib=stdc++");
 }
